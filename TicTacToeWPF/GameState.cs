e9dc878752c688b22e49700803c0949e4fe7b77d
m1azm <157737888+m1azm@users.noreply.github.com>
2025-03-20 16:14:55 +0300

    Update GameState.cs

--- TicTacToeWPF/GameState.cs
+++ TicTacToeWPF/GameState.cs
@@ -1,16 +1,9 @@
-﻿using Newtonsoft.Json;
-
-namespace TicTacToeWPF
-{
-    public class GameState
-    {
-        public string[,] Board { get; set; } = new string[3, 3];
-        public string PlayerSymbol { get; set; } = "X"; // Новое свойство
-        public string CurrentPlayer { get; set; }
-        public bool IsGameOver { get; set; }
-
-        // Вычисляемый символ компьютера
-        [JsonIgnore]
-        public string ComputerSymbol => PlayerSymbol == "X" ? "O" : "X";
-    }
-}
+﻿namespace TicTacToeWPF
+ {
+     public class GameState
+     {
+         public string[,] Board { get; set; } = new string[3, 3];
+         public string CurrentPlayer { get; set; } = "X";
+         public bool IsGameOver { get; set; }
+     }
+ }
